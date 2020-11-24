5963f81ad8c57784af45c175510490993c35bcf9
thoraf20 <thoraf20@gmail.com>
2020-11-25 00:22:55 +0100

    rcompleted two routes

---
+++ amazon-clone/backend/data.js
@@ -0,0 +1,41 @@
+//localhost:5000/api/products
+
+export default {
+    products: [
+        {
+        _id: "1",
+        name: "The Intelligent Investor",
+        category:"Non-Fiction",
+        image: "/images/book1.png",
+        price: " $50",
+        author: "Benjamin Graham",
+        rating: 4.5,
+        numReview: 10,
+        countInStock: 0
+
+    },
+    {
+        _id: "2",
+        name: "The Intelligent Investor",
+        category:"Fiction",
+        image: "/images/book1.png",
+        price: "$40",
+        author: "Benjamin Graham",
+        rating: 4.5,
+        numReview: 10,
+        countInStock: 6
+    },
+        {
+         _id: "3",
+         name: "The Intelligent Investor",
+        category:"Non-Fiction",
+        image: "/images/book1.png",
+        price: "$50",
+        author: "Benjamin Graham",
+        rating: 4.5,
+        numReview: 9,
+        countInStock: 10
+
+    },
+]
+}
