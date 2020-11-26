aa3b9f283b981992d10771bc8b1a771fe51383ab
thoraf20 <thoraf20@gmail.com>
2020-11-26 01:16:13 +0100

    more work on backend

--- amazon-clone/backend/server.js
+++ amazon-clone/backend/server.js
@@ -1,7 +1,26 @@
 import express from "express";
 import data from "./data.js";
+import config from "./config.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import userRoute from "./routes/userRoute.js";
+
+
+dotenv.config();
+
+//connection to mongodb
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    createIndexes: true    
+}).catch(error => console.log(error.reason));
 
 const app = express();
+app.use(bodyParser.json())
+
+app.use("/api/users", userRoute);
 
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
