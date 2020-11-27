92cb545433f624562203443ec08ed62427d4fd67
thoraf20 <thoraf20@gmail.com>
2020-11-27 17:53:59 +0100

    Register Screen complete

--- amazon-clone/backend/routes/userRoute.js
+++ amazon-clone/backend/routes/userRoute.js
@@ -1,7 +1,31 @@
 import express from "express";
 import User from "../model/userModel.js";
+import { getToken } from "../util.js";
 
 const router = express.Router();
+
+router.post("/register", async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    const newUser = await user.save();
+    if(newUser){
+        res.send({
+            id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+            token: getToken(newUser)
+
+        })
+
+    } else{
+        res.status({msg: "Invalid User Data "})
+    }
+})
 
 router.post("/signin", async (req, res) => {
     const signinUser = await User.findOne({
@@ -15,7 +39,7 @@ router.post("/signin", async (req, res) => {
             name: signinUser.name,
             email: signinUser.email,
             isAdmin: signinUser.isAdmin,
-            token: getToken(user)
+            token: getToken(signinUser)
         })
     } else{
         res.status({msg: "Invalid Email or Password"})
